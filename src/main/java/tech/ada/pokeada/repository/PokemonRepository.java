8d3d025874a5792838381c4da680056ca5c02619
Rodolfo Ferreira <rodolfo.lima.ferreira11@gmail.com>
2023-07-26 20:45:48 -0300

    exec1

--- src/main/java/tech/ada/pokeada/repository/PokemonRepository.java
+++ src/main/java/tech/ada/pokeada/repository/PokemonRepository.java
@@ -1,49 +1,24 @@
 package tech.ada.pokeada.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import tech.ada.pokeada.model.Pokemon;
-
-import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface PokemonRepository extends JpaRepository<Pokemon, Long> {
 
-    //Query nativa
+ /*
+ * Escreva as seguintes buscas utilizando query methods na interface PokemonRepository:
 
-    @Query(value = "SELECT * FROM POKEMON p WHERE p.name = :nome", nativeQuery = true)
-    Optional<Pokemon> findByNome(String nome);
+        * Listar Pokémons que a defense maior que o valor informado.
+        * Listar Pokémons que o firstType entre uma lista de tipos informados.
+        * Listar Pokémons que o specialAttack está entre dois valores informados.
+        * Listar Pokémons que o specialAttack é maior ou igual que o valor informado.
 
-    @Query(value = "SELECT p FROM Pokemon p where p.name = :nome")
-    Optional<Pokemon> findByNomeJpql(String nome);
+    * Escreva também buscas utilizando query nativa na interface PokemonRepository:
 
-
-    //Query Methods
-
-    Optional<Pokemon> findByNameIgnoreCase(String name);
-
-    Optional<Pokemon> findByNameIs(String name);
-
-    List<Pokemon> findByNameNot(String name);
-
-    List<Pokemon> findByFirstTypeIn(List<String> types);
-
-    List<Pokemon> findByNameLikeIgnoreCase(String name);
-
-    List<Pokemon> findByNameAndSpeedIs(String name, Integer speed);
-    List<Pokemon> findByNameOrSpeedIs(String name, Integer speed);
-
-    List<Pokemon> findBySpeedIsGreaterThan(Integer speed);
-
-    List<Pokemon> findBySpeedIsLessThan(Integer speed);
-
-    List<Pokemon> findBySpeedIsLessThanEqual(Integer speed);
-
-    List<Pokemon> findBySpeedIsGreaterThanEqual(Integer speed);
-
-    List<Pokemon> findBySpeedIsGreaterThanOrderByNameDesc(Integer speed);
+    * Obter Pokémon pelo nome.
+ * */
 
 
 }
